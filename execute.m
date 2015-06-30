1fa05bf70ece63c91a8d2834f49356a3b279e546
Roni Maimon <ronimaimon@gmail.com>
2015-06-30 18:06:36 +0300

    fix log writing

--- execute.m
+++ execute.m
@@ -1,6 +1,14 @@
 function [ output_args ] = execute( cmd, log_file )
    
-unix(cmd); dlmwrite(log_file ,cmd ,'-append');
+unix(cmd); 
+% open the file with permission to append
+fid = fopen(log_file,'a');
+myformat = '%s \n';
+% write values at end of file
+fprintf(fid, myformat, cmd);
+
+% close the file 
+fclose(fid);
 
 end
 
